0ba7521578a9b309ec9ca8711a4fd7a302339adb
Robin <robinsalminen1@gmail.com>
2024-10-23 14:59:34 +0300

    Login and main pages

---
+++ Module4App/src/pages/Login.tsx
@@ -0,0 +1,37 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonButton } from '@ionic/react';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory(); 
+
+  const handleLogin = () => {
+    console.log("Logged in with Username:", username, "and Password:", password);
+    history.push('/main'); 
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonItem>
+          <IonLabel position="floating">Username</IonLabel>
+          <IonInput value={username} onIonChange={e => setUsername(e.detail.value!)}></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Password</IonLabel>
+          <IonInput type="password" value={password} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+        </IonItem>
+        <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
